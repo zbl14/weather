9cd29f12b5ab9152c946f23c105ad000c162ba37
zbl14 <ifthereisoneday@gmail.com>
2022-06-25 22:12:21 -0700

    update enviroment setting

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: ' ',
+      title: 'Weather',
       template: './src/index.html',
       inject: 'body'
     })
