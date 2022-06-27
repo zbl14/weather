aab932abdebe0905440ed2de86ed642817548365
zbl14 <ifthereisoneday@gmail.com>
2022-06-27 10:51:32 -0700

    provide info about lon and lat, convert temperature K to F

--- src/index.js
+++ src/index.js
@@ -11,19 +11,27 @@ $(document).ready(() => {
     let request = new XMLHttpRequest();
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
-    request.onreadystatechange = () => {
+    request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
+        /* eslint-disable */
         getElements(response);
+        /* eslint-enable */
       }
     };
 
     request.open("GET", url, true);
     request.send();
-
-    getElements((response) => {
+    
+    /* eslint-disable */
+    const getElements = (response) => {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    })
+      let fahrenheitTemp= ((`${response.main.temp}` -273.15)*(9/5))+32
+      $('.showFahrenheit').text(`The temperature in Fahrenheit is ${fahrenheitTemp}`)
+      $('.showLon').text(`The longitude is ${response.coord.lon}`);
+      $('.showLat').text(`The latitude is ${response.coord.lat}`);
+    };
+    /* eslint-enable */
   });
 });
