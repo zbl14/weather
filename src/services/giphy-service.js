be06281e91f14dc60bd11a9e20e886d698f1ef57
zbl14 <ifthereisoneday@gmail.com>
2022-06-30 15:50:44 -0700

    add giphy service for search gif

--- src/services/giphy-service.js
+++ src/services/giphy-service.js
@@ -1,5 +1,5 @@
 export class GiphyService {  
-  static async getGif(query) {
+  static getGif(query) {
     return fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${process.env.GIPHY_API_KEY}&limit=5`)
       .then(function(response) {
       if (!response.ok) {
