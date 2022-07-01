fa9817f9ce5f42abec0543ec361b22d41b69b281
zbl14 <ifthereisoneday@gmail.com>
2022-06-30 21:54:15 -0700

    refactor the asyn function

--- src/services/giphy-service.js
+++ src/services/giphy-service.js
@@ -1,14 +1,13 @@
 export class GiphyService {  
-  static getGif(query) {
-    return fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${process.env.GIPHY_API_KEY}&limit=5`)
-      .then(function(response) {
+  static async getGif(query) {
+    try {
+      const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${process.env.GIPHY_API_KEY}&limit=5`);
       if (!response.ok) {
-        throw Error(response.status);
+        throw Error(response.statusText);
       }
-      return response.json();
-    })
-    .catch(function(error) {
-      return Error(error);
-    }) 
+      return await response.json();
+    } catch (error) {
+      return error.message;
+    } 
   }
 }
